262e94e432269c3db2279d4fb500153c3e579939
Tim Goodwin <timg.goodwin@gmail.com>
2025-01-20 16:03:12 -0800

    mock the emitter

--- mocks/mockgen.go
+++ mocks/mockgen.go
@@ -1,2 +1,3 @@
 //go:generate mockgen -destination=mock_client.go -package=mocks sigs.k8s.io/controller-runtime/pkg/client Client
+//go:generate mockgen -destination=mock_emitter.go -package=mocks github.com/tgoodwin/sleeve/pkg/event Emitter
 package mocks
