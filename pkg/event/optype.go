a7676c46c3e8dabfb5c917543ec3a7cb1c859f8c
Tim Goodwin <timg.goodwin@gmail.com>
2025-03-21 10:55:16 -0700

    make LogOperation public

--- pkg/event/optype.go
+++ pkg/event/optype.go
@@ -11,6 +11,13 @@ const (
 	DELETE OperationType = "DELETE"
 	PATCH  OperationType = "PATCH"
 	APPLY  OperationType = "APPLY"
+
+	// custom op types
+
+	// REMOVE is a custom operation type used to represnt an object
+	// being purged from cluster state. This is different from DELETE
+	// which simply marks an object for deletion.
+	REMOVE OperationType = "REMOVE"
 )
 
 var MutationTypes = map[OperationType]struct{}{
