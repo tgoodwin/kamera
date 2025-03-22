c8f34f4e1f97c21a10015a1bcdc4bd95065080b2
Tim Goodwin <timg.goodwin@gmail.com>
2025-03-22 08:47:56 -0700

    update unit tests

--- pkg/tracecheck/explore_test.go
+++ pkg/tracecheck/explore_test.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/tgoodwin/sleeve/pkg/snapshot"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 func Test_serializeState(t *testing.T) {
@@ -52,50 +54,82 @@ func Test_serializeState(t *testing.T) {
 }
 
 func Test_getNewPendingReconciles(t *testing.T) {
+	newPr := func(id, namespace, name string) PendingReconcile {
+		return PendingReconcile{
+			ReconcilerID: id,
+			Request: reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Namespace: namespace,
+					Name:      name,
+				},
+			},
+		}
+	}
 	tests := []struct {
 		name     string
+		mode     string
 		curr     []PendingReconcile
 		new      []PendingReconcile
 		expected []PendingReconcile
 	}{
 		{
-			name: "identical lists",
+			name: "identical lists in queue mode",
+			mode: "queue",
 			curr: []PendingReconcile{
-				{ReconcilerID: "controllerC"},
-				{ReconcilerID: "controllerB"},
+				newPr("controllerA", "namespace1", "name1"),
+				newPr("controllerB", "namespace1", "name2"),
 			},
 			new: []PendingReconcile{
-				{ReconcilerID: "controllerB"},
-				{ReconcilerID: "controllerC"},
+				newPr("controllerA", "namespace1", "name1"),
+				newPr("controllerB", "namespace1", "name2"),
 			},
 			expected: []PendingReconcile{
-				{ReconcilerID: "controllerC"},
-				{ReconcilerID: "controllerB"},
+				newPr("controllerA", "namespace1", "name1"),
+				newPr("controllerB", "namespace1", "name2"),
 			},
 		},
 		{
-			name: "one new controller",
+			name: "dedupe in queue mode",
+			mode: "queue",
 			curr: []PendingReconcile{
-				{ReconcilerID: "controllerC"},
-				{ReconcilerID: "controllerB"},
+				newPr("controllerA", "namespace1", "name1"),
+				newPr("controllerB", "namespace1", "name2"),
 			},
 			new: []PendingReconcile{
-				{ReconcilerID: "controllerB"},
-				{ReconcilerID: "controllerC"},
-				{ReconcilerID: "controllerA"},
+				newPr("controllerA", "namespace1", "name1"),
+				newPr("controllerA", "namespace1", "name2"),
 			},
 			expected: []PendingReconcile{
-				{ReconcilerID: "controllerC"},
-				{ReconcilerID: "controllerB"},
-				{ReconcilerID: "controllerA"},
+				newPr("controllerA", "namespace1", "name1"),
+				newPr("controllerB", "namespace1", "name2"),
+				newPr("controllerA", "namespace1", "name2"),
+			},
+		},
+		{
+			name: "dedupe in stack mode",
+			mode: "stack",
+			curr: []PendingReconcile{
+				newPr("controllerA", "namespace1", "name1"),
+				newPr("controllerB", "namespace1", "name2"),
+			},
+			new: []PendingReconcile{
+				newPr("controllerA", "namespace1", "name1"),
+				newPr("controllerA", "namespace1", "name2"),
+			},
+			expected: []PendingReconcile{
+				newPr("controllerA", "namespace1", "name1"),
+				newPr("controllerA", "namespace1", "name2"),
+				newPr("controllerB", "namespace1", "name2"),
 			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			actual := getNewPendingReconciles(tt.curr, tt.new)
-			if !assert.Equal(t, actual, tt.expected) {
+			c := &ExploreConfig{mode: tt.mode}
+			e := &Explorer{config: c}
+			actual := e.getNewPendingReconciles(tt.curr, tt.new)
+			if !assert.Equal(t, tt.expected, actual) {
 				t.Errorf("getNewPendingReconciles() = %v, want %v", actual, tt.expected)
 			}
 		})
