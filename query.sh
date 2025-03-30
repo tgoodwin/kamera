051d70b79edec76cceafe0d4d711df5c5d13eacd
Tim Goodwin <timg.goodwin@gmail.com>
2025-03-30 13:05:20 -0700

    helper script to parse through logs

---
+++ query.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+reconcile_prefix="$1"
+file="debug.jsonl"
+if [[ "$1" == "--file" ]]; then
+    file="$2"
+    shift 2
+fi
+
+reconcile_prefix="$1"
+mode="$2"
+
+if [[ "$mode" == "events" ]]; then
+    filter="rg -v operation_id"
+elif [[ "$mode" == "records" ]]; then
+    filter="rg operation_id | jq 'del(.. | .managedFields?)'"
+else
+    filter="cat"
+fi
+
+cat "$file" | rg "\"reconcile_id\":\"${reconcile_prefix}[^\"]*\"" | eval $filter | jq
