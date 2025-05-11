3616ba1a7d63958d45b5b4df9a874e1e5774ceee
Tim Goodwin <timg.goodwin@gmail.com>
2025-05-10 17:17:19 -0700

    add lenses

---
+++ cmd/analyze/commands/knowledge.go
@@ -0,0 +1,40 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/tgoodwin/sleeve/pkg/tracecheck"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+var stalenessCmd = &cobra.Command{
+	Use:   "staleness [trace-file] --reconcile-id <id>",
+	Short: "Analyze stale reads in controller traces",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		tracePath := args[0]
+		reconcileID, _ := cmd.Flags().GetString("reconcile-id")
+		if reconcileID == "" {
+			fmt.Println("error: --reconcile-id flag is required")
+			return
+		}
+
+		eb := tracecheck.NewExplorerBuilder(runtime.NewScheme())
+		lm, err := eb.LensManager(tracePath)
+		if err != nil {
+			fmt.Println("failed to load trace:", err)
+			return
+		}
+		err = lm.KnowledgeLens(reconcileID)
+		if err != nil {
+			fmt.Println("failed to analyze trace:", err)
+			return
+		}
+	},
+}
+
+func init() {
+	stalenessCmd.Flags().String("reconcile-id", "", "Specify the reconcile ID to analyze")
+	rootCmd.AddCommand(stalenessCmd)
+}
---
+++ cmd/analyze/commands/provenance.go
@@ -0,0 +1,40 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/tgoodwin/sleeve/pkg/tracecheck"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+var provenanceCmd = &cobra.Command{
+	Use:   "provenance [trace-file] --reconcile-id <id>",
+	Short: "Analyze stale reads in controller traces",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		tracePath := args[0]
+		objectID, _ := cmd.Flags().GetString("sleeve-object-id")
+		if objectID == "" {
+			fmt.Println("error: --sleeve-object-id flag is required")
+			return
+		}
+
+		eb := tracecheck.NewExplorerBuilder(runtime.NewScheme())
+		lm, err := eb.LensManager(tracePath)
+		if err != nil {
+			fmt.Println("failed to load trace:", err)
+			return
+		}
+		err = lm.ProvenanceLens(objectID)
+		if err != nil {
+			fmt.Println("failed to analyze trace:", err)
+			return
+		}
+	},
+}
+
+func init() {
+	provenanceCmd.Flags().String("sleeve-object-id", "", "Specify the reconcile ID to analyze")
+	rootCmd.AddCommand(stalenessCmd)
+}
