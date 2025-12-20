f70b239567a520ce5f65b8b9da1b084b3ec060fc
Tim Goodwin <timg.goodwin@gmail.com>
2025-12-20 10:43:37 -0800

    wire up depth flag to runner

--- pkg/explore/flags.go
+++ pkg/explore/flags.go
@@ -5,6 +5,7 @@ import "flag"
 var (
 	interactiveFlag = flag.Bool("interactive", true, "launch interactive trace inspector")
 	dumpPathFlag    = flag.String("dump-output", "", "optional path to write exploration results (converged + aborted) to disk")
+	depth           = flag.Int("max-explore-depth", 10, "maximum depth to explore in the state space")
 )
 
 // InteractiveEnabled returns the parsed value for the interactive flag.
