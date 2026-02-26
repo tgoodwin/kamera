fdbc836bdb52b070e395f9bce024b3834df01a2c
Tim Goodwin <timg.goodwin@gmail.com>
2026-02-25 18:04:21 -0800

    add a two step workflow for kratix

--- examples/kratix/flows.go
+++ examples/kratix/flows.go
@@ -3,26 +3,60 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/go-logr/logr"
 	kratix "github.com/syntasso/kratix/api/v1alpha1"
 	"github.com/syntasso/kratix/internal/controller"
+	"github.com/syntasso/kratix/internal/controller/controllerfakes"
 	"github.com/syntasso/kratix/lib/compression"
 	"github.com/syntasso/kratix/lib/writers"
 	"github.com/tgoodwin/kamera/pkg/coverage"
+	"github.com/tgoodwin/kamera/pkg/event"
 	"github.com/tgoodwin/kamera/pkg/explore"
 	"github.com/tgoodwin/kamera/pkg/tag"
 	"github.com/tgoodwin/kamera/pkg/tracecheck"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	fakeclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/fake"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
+	controllerconfig "sigs.k8s.io/controller-runtime/pkg/config"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
+
+const (
+	workControllerID            tracecheck.ReconcilerID = "WorkController"
+	workPlacementControllerID   tracecheck.ReconcilerID = "WorkPlacementController"
+	promiseControllerID         tracecheck.ReconcilerID = "PromiseController"
+	promiseRevisionControllerID tracecheck.ReconcilerID = "PromiseRevisionController"
+)
+
+const (
+	workKind            = "platform.kratix.io/Work"
+	workPlacementKind   = "platform.kratix.io/WorkPlacement"
+	destinationKind     = "platform.kratix.io/Destination"
+	stateStoreKind      = "platform.kratix.io/BucketStateStore"
+	resourceBindingKind = "platform.kratix.io/ResourceBinding"
+	promiseKind         = "platform.kratix.io/Promise"
+	promiseRevisionKind = "platform.kratix.io/PromiseRevision"
+)
+
+type dynamicControllerSpec struct {
+	key          string
+	controllerID tracecheck.ReconcilerID
+	promiseName  string
+	gvk          *schema.GroupVersionKind
+	crd          *apiextensionsv1.CustomResourceDefinition
+	placeholder  *controller.DynamicResourceRequestController
+}
 
 type noopScheduler struct{}
 
@@ -58,22 +92,19 @@ func init() {
 	)
 }
 
-func buildWorksFlow() (*tracecheck.ExplorerBuilder, tracecheck.StateNode, error) {
+func newKratixExplorerBuilder() *tracecheck.ExplorerBuilder {
 	scheme := runtime.NewScheme()
 	utilruntime.Must(kratix.AddToScheme(scheme))
-
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(kratix.AddToScheme(clientgoscheme.Scheme))
 	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
 	tracecheck.SetLogger(ctrl.Log.WithName("tracecheck"))
-
-	const workControllerID = "WorkController"
-	const workPlacementControllerID = "WorkPlacementController"
-	workKind := "platform.kratix.io/Work"
-	workPlacementKind := "platform.kratix.io/WorkPlacement"
-	destinationKind := "platform.kratix.io/Destination"
-	stateStoreKind := "platform.kratix.io/BucketStateStore"
-
 	eb := tracecheck.NewExplorerBuilder(scheme)
 	eb.WithMaxDepth(100)
+	return eb
+}
+
+func configureWorksReconcilers(eb *tracecheck.ExplorerBuilder) {
 	eb.WithReconciler(workControllerID, func(c ctrlclient.Client) tracecheck.Reconciler {
 		return &controller.WorkReconciler{
 			Client:        c,
@@ -94,6 +125,57 @@ func buildWorksFlow() (*tracecheck.ExplorerBuilder, tracecheck.StateNode, error)
 	eb.WithResourceDep(workPlacementKind, workPlacementControllerID)
 	eb.WithResourceDep(destinationKind, workPlacementControllerID)
 	eb.WithResourceDep(stateStoreKind, workPlacementControllerID)
+}
+
+func configurePromisesReconcilers(
+	eb *tracecheck.ExplorerBuilder,
+	preStarted map[string]*controller.DynamicResourceRequestController,
+) {
+	eb.WithReconciler(promiseControllerID, func(c ctrlclient.Client) tracecheck.Reconciler {
+		nsClient := &defaultNamespaceClient{Client: c, namespace: "default"}
+		manager := &controllerfakes.FakeManager{}
+		skipNameValidation := true
+		manager.GetControllerOptionsReturns(controllerconfig.Controller{SkipNameValidation: &skipNameValidation})
+		manager.GetEventRecorderForReturns(record.NewFakeRecorder(32))
+		return &controller.PromiseReconciler{
+			Client:                    nsClient,
+			ApiextensionsClient:       fakeclientset.NewSimpleClientset().ApiextensionsV1(),
+			Log:                       ctrl.Log.WithName("promise"),
+			Manager:                   manager,
+			EventRecorder:             record.NewFakeRecorder(32),
+			PromiseUpgrade:            true,
+			NumberOfJobsToKeep:        1,
+			ReconciliationInterval:    time.Hour,
+			RestartManager:            func() {},
+			StartedDynamicControllers: copyDynamicControllers(preStarted),
+		}
+	}).For(promiseKind)
+	eb.WithReconciler(promiseRevisionControllerID, func(c ctrlclient.Client) tracecheck.Reconciler {
+		nsClient := &defaultNamespaceClient{Client: c, namespace: "default"}
+		return &controller.PromiseRevisionReconciler{
+			Client:        nsClient,
+			Log:           ctrl.Log.WithName("promise-revision"),
+			EventRecorder: record.NewFakeRecorder(32),
+		}
+	}).For(promiseRevisionKind)
+	eb.WithResourceDep(promiseKind, promiseControllerID)
+	eb.WithResourceDep(promiseRevisionKind, promiseRevisionControllerID)
+}
+
+func buildInputDrivenBuilder(inputs []coverage.Input) (*tracecheck.ExplorerBuilder, error) {
+	specs := dynamicControllerSpecsFromInputs(inputs)
+	prestarted := preStartedDynamicControllersFromSpecs(specs)
+
+	eb := newKratixExplorerBuilder()
+	configureWorksReconcilers(eb)
+	configureDynamicRequestReconcilers(eb, specs)
+	configurePromisesReconcilers(eb, prestarted)
+	return eb, nil
+}
+
+func buildWorksFlow() (*tracecheck.ExplorerBuilder, tracecheck.StateNode, error) {
+	eb := newKratixExplorerBuilder()
+	configureWorksReconcilers(eb)
 
 	work := &kratix.Work{
 		ObjectMeta: metav1.ObjectMeta{
@@ -200,42 +282,8 @@ func buildWorksFlow() (*tracecheck.ExplorerBuilder, tracecheck.StateNode, error)
 }
 
 func buildPromisesFlow() (*tracecheck.ExplorerBuilder, tracecheck.StateNode, error) {
-	scheme := runtime.NewScheme()
-	utilruntime.Must(kratix.AddToScheme(scheme))
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-	utilruntime.Must(kratix.AddToScheme(clientgoscheme.Scheme))
-
-	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
-	tracecheck.SetLogger(ctrl.Log.WithName("tracecheck"))
-
-	const promiseControllerID = "PromiseController"
-	const promiseRevisionControllerID = "PromiseRevisionController"
-	promiseKind := "platform.kratix.io/Promise"
-	promiseRevisionKind := "platform.kratix.io/PromiseRevision"
-
-	eb := tracecheck.NewExplorerBuilder(scheme)
-	eb.WithMaxDepth(100)
-	eb.WithReconciler(promiseControllerID, func(c ctrlclient.Client) tracecheck.Reconciler {
-		nsClient := &defaultNamespaceClient{Client: c, namespace: "default"}
-		return &controller.PromiseReconciler{
-			Client:                 nsClient,
-			Log:                    ctrl.Log.WithName("promise"),
-			EventRecorder:          record.NewFakeRecorder(32),
-			PromiseUpgrade:         true,
-			NumberOfJobsToKeep:     1,
-			ReconciliationInterval: time.Hour,
-		}
-	}).For(promiseKind)
-	eb.WithReconciler(promiseRevisionControllerID, func(c ctrlclient.Client) tracecheck.Reconciler {
-		nsClient := &defaultNamespaceClient{Client: c, namespace: "default"}
-		return &controller.PromiseRevisionReconciler{
-			Client:        nsClient,
-			Log:           ctrl.Log.WithName("promise-revision"),
-			EventRecorder: record.NewFakeRecorder(32),
-		}
-	}).For(promiseRevisionKind)
-	eb.WithResourceDep(promiseKind, promiseControllerID)
-	eb.WithResourceDep(promiseRevisionKind, promiseRevisionControllerID)
+	eb := newKratixExplorerBuilder()
+	configurePromisesReconcilers(eb, nil)
 
 	promise := &kratix.Promise{
 		ObjectMeta: metav1.ObjectMeta{
@@ -268,6 +316,303 @@ func buildPromisesFlow() (*tracecheck.ExplorerBuilder, tracecheck.StateNode, err
 	return eb, initialState, nil
 }
 
-func scenariosFromInputs(_ *tracecheck.ExplorerBuilder, _ []coverage.Input) ([]explore.Scenario, error) {
-	return nil, fmt.Errorf("input to scenario conversion not implemented")
+func scenariosFromInputs(builder *tracecheck.ExplorerBuilder, inputs []coverage.Input) ([]explore.Scenario, error) {
+	if builder == nil {
+		return nil, fmt.Errorf("builder is nil")
+	}
+	if len(inputs) == 0 {
+		return nil, fmt.Errorf("no inputs supplied")
+	}
+
+	scenarios := make([]explore.Scenario, 0, len(inputs))
+	for idx, input := range inputs {
+		state, seededObjects, err := buildStateFromCoverageInput(builder, input)
+		if err != nil {
+			return nil, fmt.Errorf("build start state for input %d (%s): %w", idx, input.Name, err)
+		}
+		userInputs, err := buildUserActionsFromCoverageInput(input, seededObjects)
+		if err != nil {
+			return nil, fmt.Errorf("build user actions for input %d (%s): %w", idx, input.Name, err)
+		}
+
+		scenarios = append(scenarios, explore.Scenario{
+			Name:             input.Name,
+			EnvironmentState: state,
+			UserInputs:       userInputs,
+			Config:           applyInputTuning(builder.Config(), input.Tuning),
+		})
+	}
+
+	if len(scenarios) == 0 {
+		return nil, fmt.Errorf("no scenarios produced")
+	}
+	return scenarios, nil
+}
+
+func buildStateFromCoverageInput(
+	builder *tracecheck.ExplorerBuilder,
+	input coverage.Input,
+) (tracecheck.StateNode, []ctrlclient.Object, error) {
+	objects := make([]ctrlclient.Object, 0, len(input.EnvironmentState.Objects))
+	for _, obj := range input.EnvironmentState.Objects {
+		if obj == nil {
+			continue
+		}
+		objects = append(objects, obj.DeepCopy())
+	}
+	if len(objects) == 0 {
+		return tracecheck.StateNode{}, nil, fmt.Errorf("input has no environment objects")
+	}
+
+	pending := initialPendingReconciles(objects)
+	state, err := builder.BuildStartStateFromObjects(objects, pending)
+	if err != nil {
+		return tracecheck.StateNode{}, nil, err
+	}
+	return state, objects, nil
+}
+
+func initialPendingReconciles(objects []ctrlclient.Object) []tracecheck.PendingReconcile {
+	pending := make([]tracecheck.PendingReconcile, 0)
+	for _, obj := range objects {
+		if obj == nil {
+			continue
+		}
+		controllerID, ok := controllerForObject(obj)
+		if !ok {
+			continue
+		}
+		pending = append(pending, tracecheck.PendingReconcile{
+			ReconcilerID: controllerID,
+			Request: ctrl.Request{
+				NamespacedName: ctrlclient.ObjectKeyFromObject(obj),
+			},
+			Source: tracecheck.SourceStateChange,
+		})
+	}
+	return pending
+}
+
+func controllerForObject(obj ctrlclient.Object) (tracecheck.ReconcilerID, bool) {
+	gvk := obj.GetObjectKind().GroupVersionKind()
+	switch gvk.Group + "/" + gvk.Kind {
+	case "platform.kratix.io/Promise":
+		return promiseControllerID, true
+	case "platform.kratix.io/PromiseRevision":
+		return promiseRevisionControllerID, true
+	case "platform.kratix.io/Work":
+		return workControllerID, true
+	case "platform.kratix.io/WorkPlacement", "platform.kratix.io/Destination", "platform.kratix.io/BucketStateStore":
+		return workPlacementControllerID, true
+	default:
+		return "", false
+	}
+}
+
+func dynamicControllerSpecsFromInputs(inputs []coverage.Input) []dynamicControllerSpec {
+	out := make([]dynamicControllerSpec, 0)
+	seen := make(map[string]struct{})
+	add := func(obj *unstructured.Unstructured) {
+		spec, ok := dynamicControllerSpecForPromise(obj)
+		if !ok {
+			return
+		}
+		if _, exists := seen[spec.key]; exists {
+			return
+		}
+		seen[spec.key] = struct{}{}
+		out = append(out, spec)
+	}
+
+	for _, input := range inputs {
+		for _, obj := range input.EnvironmentState.Objects {
+			add(obj)
+		}
+		for _, userInput := range input.UserInputs {
+			add(userInput.Object)
+		}
+	}
+	return out
+}
+
+func dynamicControllerSpecForPromise(obj *unstructured.Unstructured) (dynamicControllerSpec, bool) {
+	if obj == nil {
+		return dynamicControllerSpec{}, false
+	}
+	if obj.GetKind() != "Promise" || !strings.HasPrefix(obj.GetAPIVersion(), "platform.kratix.io/") {
+		return dynamicControllerSpec{}, false
+	}
+
+	promise := &kratix.Promise{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, promise); err != nil {
+		return dynamicControllerSpec{}, false
+	}
+	if !promise.ContainsAPI() {
+		return dynamicControllerSpec{}, false
+	}
+
+	gvk, crd, err := promise.GetAPI()
+	if err != nil || gvk == nil || crd == nil {
+		return dynamicControllerSpec{}, false
+	}
+
+	controllerID := tracecheck.ReconcilerID(fmt.Sprintf("DynamicResourceRequestController/%s", promise.GetName()))
+	return dynamicControllerSpec{
+		key:          promise.GetDynamicControllerName(ctrl.Log.WithName("promise-prestart")),
+		controllerID: controllerID,
+		promiseName:  promise.GetName(),
+		gvk:          gvk,
+		crd:          crd,
+		placeholder:  &controller.DynamicResourceRequestController{},
+	}, true
+}
+
+func preStartedDynamicControllersFromSpecs(specs []dynamicControllerSpec) map[string]*controller.DynamicResourceRequestController {
+	if len(specs) == 0 {
+		return nil
+	}
+	out := make(map[string]*controller.DynamicResourceRequestController, len(specs))
+	for _, spec := range specs {
+		out[spec.key] = spec.placeholder
+	}
+	return out
+}
+
+func configureDynamicRequestReconcilers(eb *tracecheck.ExplorerBuilder, specs []dynamicControllerSpec) {
+	for _, spec := range specs {
+		spec := spec
+		rrKind := spec.gvk.Group + "/" + spec.gvk.Kind
+
+		eb.WithReconciler(spec.controllerID, func(c ctrlclient.Client) tracecheck.Reconciler {
+			nsClient := &defaultNamespaceClient{Client: c, namespace: "default"}
+			enabled := true
+			canCreateResources := true
+
+			controllerValue := controller.DynamicResourceRequestController{
+				Client:                      nsClient,
+				GVK:                         spec.gvk,
+				Scheme:                      c.Scheme(),
+				PromiseIdentifier:           spec.promiseName,
+				Log:                         ctrl.Log.WithName("dynamic-resource-request").WithName(spec.promiseName),
+				UID:                         "00000",
+				Enabled:                     &enabled,
+				CRD:                         spec.crd,
+				PromiseDestinationSelectors: nil,
+				CanCreateResources:          &canCreateResources,
+				NumberOfJobsToKeep:          1,
+				ReconciliationInterval:      time.Hour,
+				EventRecorder:               record.NewFakeRecorder(32),
+				PromiseUpgrade:              true,
+			}
+			*spec.placeholder = controllerValue
+			return spec.placeholder
+		}).For(rrKind)
+
+		eb.WithResourceDep(rrKind, spec.controllerID)
+		eb.WithResourceDep(workKind, spec.controllerID)
+		eb.WithResourceDep(resourceBindingKind, spec.controllerID)
+	}
+}
+
+func copyDynamicControllers(
+	in map[string]*controller.DynamicResourceRequestController,
+) map[string]*controller.DynamicResourceRequestController {
+	if len(in) == 0 {
+		return nil
+	}
+	out := make(map[string]*controller.DynamicResourceRequestController, len(in))
+	for k, v := range in {
+		out[k] = v
+	}
+	return out
+}
+
+func applyInputTuning(base tracecheck.ExploreConfig, tuning coverage.InputTuning) tracecheck.ExploreConfig {
+	cfg := base.Clone()
+	if tuning.MaxDepth > 0 {
+		cfg.MaxDepth = tuning.MaxDepth
+	}
+	if len(tuning.PermuteControllers) > 0 {
+		if cfg.Perturbations.PermuteOrder == nil {
+			cfg.Perturbations.PermuteOrder = make(map[tracecheck.ReconcilerID]bool)
+		}
+		for _, controllerID := range tuning.PermuteControllers {
+			cfg.Perturbations.PermuteOrder[tracecheck.ReconcilerID(controllerID)] = true
+		}
+	}
+	if len(tuning.StaleReads) > 0 {
+		if cfg.Perturbations.Staleness == nil {
+			cfg.Perturbations.Staleness = make(map[tracecheck.ReconcilerID]tracecheck.StalenessConfig)
+		}
+		for controllerID, kinds := range tuning.StaleReads {
+			id := tracecheck.ReconcilerID(controllerID)
+			staleness := cfg.Perturbations.Staleness[id]
+			if staleness.StaleReadBounds == nil {
+				staleness.StaleReadBounds = make(tracecheck.LookbackLimits)
+			}
+			for _, kind := range kinds {
+				trimmed := strings.TrimSpace(kind)
+				if trimmed == "" {
+					continue
+				}
+				lookback := tuning.StaleLookback[trimmed]
+				if lookback <= 0 {
+					lookback = 1
+				}
+				staleness.StaleReadBounds[trimmed] = tracecheck.LookbackLimit(lookback)
+			}
+			cfg.Perturbations.Staleness[id] = staleness
+		}
+	}
+	return cfg
+}
+
+func buildUserActionsFromCoverageInput(
+	input coverage.Input,
+	seededObjects []ctrlclient.Object,
+) ([]tracecheck.UserAction, error) {
+	actions := make([]tracecheck.UserAction, 0, len(input.UserInputs))
+	for idx, action := range input.UserInputs {
+		if action.Object == nil {
+			return nil, fmt.Errorf("input user input %d has nil object", idx)
+		}
+		id := strings.TrimSpace(action.ID)
+		if id == "" {
+			id = fmt.Sprintf("user-input-%d", idx)
+		}
+		opType := action.Type
+		if opType == event.CREATE && isInputObjectSeeded(action.Object, seededObjects) {
+			opType = event.UPDATE
+		}
+		actions = append(actions, tracecheck.UserAction{
+			ID:      id,
+			OpType:  opType,
+			Payload: action.Object.DeepCopy(),
+		})
+	}
+	return actions, nil
+}
+
+func isInputObjectSeeded(object ctrlclient.Object, seededObjects []ctrlclient.Object) bool {
+	if object == nil {
+		return false
+	}
+	for _, seeded := range seededObjects {
+		if sameObjectIdentity(seeded, object) {
+			return true
+		}
+	}
+	return false
+}
+
+func sameObjectIdentity(a, b ctrlclient.Object) bool {
+	if a == nil || b == nil {
+		return false
+	}
+	aGVK := a.GetObjectKind().GroupVersionKind()
+	bGVK := b.GetObjectKind().GroupVersionKind()
+	if aGVK.Group != bGVK.Group || aGVK.Kind != bGVK.Kind {
+		return false
+	}
+	return a.GetNamespace() == b.GetNamespace() && a.GetName() == b.GetName()
 }
