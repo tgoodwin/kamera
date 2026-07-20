35f4b15881e0eb2d66e805d7d62d3b76d55d4df6
Tim Goodwin <timg.goodwin@gmail.com>
2026-07-20 12:37:30 +0100

    match table 6 output format

--- artifact/run-table6.sh
+++ artifact/run-table6.sh
@@ -33,26 +33,28 @@ done
 
 awk -F '\t' '
   BEGIN {
-    print "# Table 6: perturbed simulation execution time"
+    print "# Table 6: bug reproduction time"
     print ""
     print "Only the Kamera perturbed-run duration is freshly measured. Sieve times are the paper baselines and are not rerun."
     print ""
-    print "| Experiment | Paper Sieve (s) | Paper Kamera (ms) | Observed Kamera (ms) | Observed / paper | Reproduced speedup over Sieve | Status |"
-    print "|---|---:|---:|---:|---:|---:|---|"
+    print "| Controller | Bug | Sieve | Kamera | Speedup |"
+    print "|---|---|---:|---:|---:|"
   }
   NR > 1 {
-    ratio = $4 / $2
+    split($1, experiment, "/")
+    if (experiment[1] == "zk") controller = "ZooKeeper"
+    else if (experiment[1] == "rmq") controller = "RabbitMQ"
+    else if (experiment[1] == "cass") controller = "Cassandra"
+    else controller = experiment[1]
+
     speedup = ($3 * 1000) / $4
-    printf "| %s | %.0f | %.0f | %.3f | %.2fx | %.0fx | %s |\n", $1, $3, $2, $4, ratio, speedup, $5
-    logsum += log(ratio)
+    printf "| %s | %s | %.0f s | %.3f ms | %.0fx |\n", controller, experiment[2], $3, $4, speedup
     speedup_logsum += log(speedup)
     count++
   }
   END {
-    print ""
     if (count > 0) {
-      printf "Geometric mean reproduced speedup over Sieve: %.0fx\n", exp(speedup_logsum/count)
-      printf "Geometric mean observed/paper Kamera ratio: %.2fx\n", exp(logsum/count)
+      printf "| **Geometric mean** |  |  |  | **%.0fx** |\n", exp(speedup_logsum/count)
     }
   }
 ' "$tsv" >"$output_root/table6.md"
